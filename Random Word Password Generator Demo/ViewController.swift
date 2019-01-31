a23efea164462d5ff153b1bceb6e9126a447e6c0
Mirko Cukich <mirkocukich@gmail.com>
2019-01-31 16:51:44 -0600

    Storyboard elements connected to ViewController

--- Random Word Password Generator Demo/ViewController.swift
+++ Random Word Password Generator Demo/ViewController.swift
@@ -9,12 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var switchController: UISwitch!
+    
+    var selectedText: String? = "Password"
+    // Random Words Array  ######## come back to finish
+    let wordArray = ["","","",""]
+    // Random Passwords Array  ######## come back to finish
+    let passwordArray = ["A","B","C"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // Create Single Example Button -> Gives Single Example
+    @IBAction func createBtn(_ sender: UIButton) {
+        
+    }
+    // Switch option for which item to demo:
+    // Random Words or Password Character Generator
+    @IBAction func changeSwitch(_ sender: UISwitch) {
+        
+    }
+    // Number taged buttons which demo amount of items by number selected
+    // 1 item selected to be demoed
+    @IBAction func clicked01Btn(_ sender: UIButton) {
+        
+    }
+    // 2 items selected to be demoed
+    @IBAction func clicked02Btn(_ sender: UIButton) {
+        
+    }
+    // 3 items selected to be demoed
+    @IBAction func clicked03Btn(_ sender: UIButton) {
+        
+    }
+    // 4 items selected to be demoed
+    @IBAction func clicked04Btn(_ sender: UIButton) {
+        
+    }
+    // 5 items selected to be demoed
+    @IBAction func clicked05Btn(_ sender: UIButton) {
+        
+    }
+    // 6 items selected to be demoed
+    @IBAction func clicked06Btn(_ sender: UIButton) {
+        
+    }
+    // 7 items selected to be demoed
+    @IBAction func clicked07Btn(_ sender: UIButton) {
+        
+    }
+    // 8 items selected to be demoed
+    @IBAction func clicked08Btn(_ sender: UIButton) {
+        
+    }
+    // 9 items selected to be demoed
+    @IBAction func clicked09Btn(_ sender: UIButton) {
+        
+    }
+    // Resets the label back to its default text
+    @IBAction func clearBtn(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
 
