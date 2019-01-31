37142566d72c121250108e47141474f252904455
Mirko Cukich <mirkocukich@gmail.com>
2019-01-31 17:03:48 -0600

    Arrays populated with information to be used in app

--- Random Word Password Generator Demo/ViewController.swift
+++ Random Word Password Generator Demo/ViewController.swift
@@ -14,10 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var switchController: UISwitch!
     
     var selectedText: String? = "Password"
-    // Random Words Array  ######## come back to finish
-    let wordArray = ["","","",""]
-    // Random Passwords Array  ######## come back to finish
-    let passwordArray = ["A","B","C"]
+    // Random Words Array, demo words found on Lorem Ipsum
+    let wordArray = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing",
+                     "elit", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore",
+                     "et", "dolore", "magna", "aliqua"]
+    // Random Passwords Array, assorted characters to be used
+    let passwordArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                         "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                         "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#",
+                         "$", "%", "&", "*", "-", "_"]
     
 
     override func viewDidLoad() {
